a92f98e67ab557b3b18b47acedc568ea42a2eeef
paolajdelgado <delgadog.paola@gmail.com>
2024-12-19 17:49:01 -0800

    Cambios

--- Grupo_1_Delgado_Higuera_Rodriguez/trabajoPracticoIntegrador.js
+++ Grupo_1_Delgado_Higuera_Rodriguez/trabajoPracticoIntegrador.js
@@ -1,21 +1,23 @@
 
+
+
 let libros = [
-    { id: 01, titulo: "Los juegos del hambre", autor: "Suzanne Collins" , año: 2008, genero: "Ciencia ficción", disponible: true },
-    { id: 02, titulo: "Orgullo y prejuicio", autor: "Jane Austen", año: 1813, genero: "Novela", disponible: false },
-    { id: 03, titulo: "Introducción a los algoritmos", autor: "Thomas H. Cormen", año: 1990, genero: "Programacion", disponible: true },
-    { id: 04, titulo: "Los cuatro acuerdos", autor: "Miguel Ángel Ruiz", año: 1997, genero: "Filosofía", disponible: true },
-    { id: 05, titulo: "Emociones que matan: La conexión entre sus emociones y su salud", autor: "Dr. Colbert", año: 2006, genero: "Autoayuda", disponible: false},
-    { id: 06, titulo: "La maestría del amor", autor: "Miguel Ángel Ruiz", año: 1999, genero: "Filosofía", disponible: false },
-    { id: 07, titulo: "La biblioteca de la medianoche", autor: "Matt Haig", año: 2020, genero: "", disponible: false },
-    { id: 08, titulo: "Hábitos atómicos", autor: "James Clear", año: 2018, genero: "Autoayuda", disponible: true },
-    { id: 09, titulo: "1984", autor: "George Orwell", año: 1949, genero: "Ciencia ficción", disponible: false },
+    { id: 1, titulo: "Los juegos del hambre", autor: "Suzanne Collins" , año: 2008, genero: "Ciencia ficción", disponible: true },
+    { id: 2, titulo: "Orgullo y prejuicio", autor: "Jane Austen", año: 1813, genero: "Novela", disponible: false },
+    { id: 3, titulo: "Introducción a los algoritmos", autor: "Thomas H. Cormen", año: 1990, genero: "Programacion", disponible: true },
+    { id: 4, titulo: "Los cuatro acuerdos", autor: "Miguel Ángel Ruiz", año: 1997, genero: "Filosofía", disponible: true },
+    { id: 5, titulo: "Emociones que matan: La conexión entre sus emociones y su salud", autor: "Dr. Colbert", año: 2006, genero: "Autoayuda", disponible: false},
+    { id: 6, titulo: "La maestría del amor", autor: "Miguel Ángel Ruiz", año: 1999, genero: "Filosofía", disponible: false },
+    { id: 7, titulo: "La biblioteca de la medianoche", autor: "Matt Haig", año: 2020, genero: "", disponible: false },
+    { id: 8, titulo: "Hábitos atómicos", autor: "James Clear", año: 2018, genero: "Autoayuda", disponible: true },
+    { id: 9, titulo: "1984", autor: "George Orwell", año: 1949, genero: "Ciencia ficción", disponible: false },
     { id: 10, titulo: "Guía de campo de los tiburones del mundo", autor: "Leonard Compagno", año: 2006, genero: "Biología", disponible: true }
 ];
 
 let usuarios = [
-    { id: 01, nombre: "Emma", email: "emma07@mail.com", librosPrestados: "" },
-    { id: 02, nombre: "Blanca", email: "Blanca04@mail.com", librosPrestados: "" },
-    { id: 03, nombre: "Esteban", email: "este_bandido70@mail.com", librosPrestados: "" },
-    { id: 04, nombre: "Blanca", email: "Blanca04@mail.com", librosPrestados: "" },
-    { id: 05, nombre: "Blanca", email: "Blanca04@mail.com", librosPrestados: "" }
+    { id: 1, nombre: "Emma", email: "emma07@mail.com", librosPrestados: "" },
+    { id: 2, nombre: "Blanca", email: "Blanca04@mail.com", librosPrestados: "" },
+    { id: 3, nombre: "Esteban", email: "este_bandido70@mail.com", librosPrestados: "" },
+    { id: 4, nombre: "Blanca", email: "Blanca04@mail.com", librosPrestados: "" },
+    { id: 5, nombre: "Blanca", email: "Blanca04@mail.com", librosPrestados: "" }
 ];
